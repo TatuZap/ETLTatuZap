02bf59a6230af58488e9b0f9bbbe3a72d847281c
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-10-21 20:02:59 -0300

    fix: wrong file separator for Linux

--- src/extract/get_files.py
+++ src/extract/get_files.py
@@ -1,10 +1,12 @@
 import os
 import pandas as pd
 
+# Determines the file separator depending on OS ('\\' for Windows and '/' for Linux)
+directory_separator = '\\' if os.name == 'nt' else '/'
+
 def files():
     path=os.path.realpath('../files')
     files_name=os.listdir(path)
     files = []
-    for file in files_name: files.append(path + '/' +file)
+    for file in files_name: files.append(path + directory_separator + file)
     return files
-
