66b96e5942e5165c2008deaa9bc4dbd4f9ec08e4
joaomendonca30 <joao.mendonca@aluno.ufabc.edu.br>
2022-10-26 19:26:06 -0300

    Conexão realizada com o MongoDB

--- src/extract/classes_by_ra_query.py
+++
@@ -1,6 +0,0 @@
-# TODO create queries folder (child of src) where this file will be
-import tratamento
-
-dataframes = tratamento.dfs
-
-print('TODO: Fazer query/queries após os tratamentos estarem concertados')
--- src/main.py
+++
@@ -1,3 +0,0 @@
-from extract.tratamento import tratamento
-
-tratamento()
