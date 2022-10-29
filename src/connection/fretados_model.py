0c963b8ca8cf9c6439aa77f19e45bb9ba314bfbc
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-10-29 19:17:33 -0300

    feat: implement populate_database method

--- src/connection/fretados_model.py
+++ src/connection/fretados_model.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from urllib import response
 from database import get_db, DBconfig
+import json
+from raspador_fretados import tables_on_page, clean_bus_df
 
 @dataclass
 class FretadoModel:
@@ -13,15 +15,14 @@ class FretadoModel:
             retorna todos os fretados
         """
 
-        return self.__get_collection().find() 
-
+        return self.__get_collection().find()
 
     def next_bus_sa_sbc(self, horario):
         return self.__get_collection().find( { "santo andre partida" : { "$exists" : True } , "sao bernardo chegada" : { "$exists" : True } })
 
     def find_by_linha(self, linha):
         return self.__get_collection().findOne({ "linha": linha })
-        
+
     def insert_item(self, item):
         return self.__get_collection().insert_one(item)
 
@@ -31,6 +32,16 @@ class FretadoModel:
     def delete_all(self):
         return self.__get_collection().drop()
 
+    def populate_database(self):
+        # deleta o conteúdo atual do banco
+        self.delete_all()
+
+        # preparando as tabelas para inseri-las elemento a elemento no banco
+        fretados_json = [ json.loads(table.to_json(orient='records')) for table in clean_bus_df(tables_on_page) ]
+
+        # inserção multipla
+        for table_json in fretados_json:
+            self.insert_items(table_json)
+
     def __get_collection(self):
         return get_db.get_collection("fretados")
-
--- src/connection/populate-fretados.py
+++ src/connection/populate-fretados.py
@@ -9,18 +9,9 @@ fretados_model = FretadoModel()
 # for item in fretados_model.list_all():
 #     print(item)
 
-# preparando as tabelas para inseri-las elemento a elemento no banco
-# fretados_json = [ json.loads(table.to_json(orient='records')) for table in clean_bus_df(tables_on_page) ]
-
-# # inserao multipla
-# for table_json in fretados_json:
-#     fretados_model.insert_items(table_json)
-
-
+# resetando o banco (deletando tudo e depois raspando e inserindo os dados)
+fretados_model.populate_database()
 
 # listando os que saem de SA e vão para SBC
 for item in fretados_model.next_bus_sa_sbc(""):
     print(item)
-
-
-#print(a==b)
